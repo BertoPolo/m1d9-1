1b00fdb21be033c6937a1e8455a3b740e8f4cf7b
BertoPolo <bertopolo91@gmail.com>
2021-11-04 15:35:05 +0100

    tryin to create divs

--- fknjs.js
+++ fknjs.js
@@ -1,1 +1,35 @@
-let
+/*const displaySpots = function() {
+    
+    let bingoSpotsNode = document.getElementById("bingoSpots")
+
+    for ( let spotNumber = 1 ; spotNumber <= 76; spotNumber++ ){
+        let newSpotNumber = document.createElement("div")
+        newSpotNumber.innerText = spotNumber
+        newSpotNumber.classList.add("spots")
+
+        spotNumber.appendChild(newSpotNumber)
+    }
+}
+window.onload = function() {
+    displaySpots()
+}
+*/
+
+const displayMonth = function() {
+
+       let monthContainerNode = document.getElementById("bingoSpots")
+
+    for (let dayNumber = 1; dayNumber <= 76; dayNumber++) {
+
+        // We create a new DIV element...
+        let newDayNode = document.createElement("div") 
+        newDayNode.classList.add("day") 
+
+        monthContainerNode.appendChild(newDayNode)
+    }
+
+}
+
+window.onload = function() {
+    displayMonth()
+}
